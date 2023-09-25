b386f42eb56de2969ca08f245602f0946efe763d
roodev <rodrigo.fessore@gmail.com>
2023-09-25 11:17:18 -0300

    Conclusão do desafio de projeto Sistema de Hospedagem em Hotel

--- Models/Reserva.cs
+++ Models/Reserva.cs
@@ -1,60 +1,57 @@
 namespace DesafioProjetoHospedagem.Models
 {
-    public class Reserva
-    {
-        public List<Pessoa> Hospedes { get; set; }
-        public Suite Suite { get; set; }
-        public int DiasReservados { get; set; }
+	public class Reserva
+	{
+		public List<Pessoa> Hospedes { get; set; }
+		public Suite Suite { get; set; }
+		public int DiasReservados { get; set; }
 
-        public Reserva() { }
+		public Reserva() { }
 
-        public Reserva(int diasReservados)
-        {
-            DiasReservados = diasReservados;
-        }
+		public Reserva(int diasReservados)
+		{
+			DiasReservados = diasReservados;
+		}
 
-        public void CadastrarHospedes(List<Pessoa> hospedes)
-        {
-            // TODO: Verificar se a capacidade é maior ou igual ao número de hóspedes sendo recebido
-            // *IMPLEMENTE AQUI*
-            if (true)
-            {
-                Hospedes = hospedes;
-            }
-            else
-            {
-                // TODO: Retornar uma exception caso a capacidade seja menor que o número de hóspedes recebido
-                // *IMPLEMENTE AQUI*
-            }
-        }
+		public void CadastrarHospedes(List<Pessoa> hospedes)
+		{
+			// TODO: Verificar se a capacidade é maior ou igual ao número de hóspedes sendo recebido
+			if (Suite.Capacidade >= hospedes.Count)
+			{
+				Hospedes = hospedes;
+			}
+			else
+			{
+				// TODO: Retornar uma exception caso a capacidade seja menor que o número de hóspedes recebido
+				throw new Exception($"Não foi possível efetuar a reserva. Você deseja acomodar {hospedes.Count} hóspedes e a suíte selecionada comporta apenas {Suite.Capacidade} hóspedes");
+			}
+		}
 
-        public void CadastrarSuite(Suite suite)
-        {
-            Suite = suite;
-        }
+		public void CadastrarSuite(Suite suite)
+		{
+			Suite = suite;
+		}
 
-        public int ObterQuantidadeHospedes()
-        {
-            // TODO: Retorna a quantidade de hóspedes (propriedade Hospedes)
-            // *IMPLEMENTE AQUI*
-            return 0;
-        }
+		public int ObterQuantidadeHospedes()
+		{
+			// TODO: Retorna a quantidade de hóspedes (propriedade Hospedes)
+			return Hospedes.Count;
+		}
 
-        public decimal CalcularValorDiaria()
-        {
-            // TODO: Retorna o valor da diária
-            // Cálculo: DiasReservados X Suite.ValorDiaria
-            // *IMPLEMENTE AQUI*
-            decimal valor = 0;
+		public decimal CalcularValorDiaria()
+		{
+			// TODO: Retorna o valor da diária
+			decimal valorIntegral = DiasReservados * Suite.ValorDiaria;
+			decimal valor = valorIntegral;
 
-            // Regra: Caso os dias reservados forem maior ou igual a 10, conceder um desconto de 10%
-            // *IMPLEMENTE AQUI*
-            if (true)
-            {
-                valor = 0;
-            }
+			// Regra: Caso os dias reservados forem maior ou igual a 10, conceder um desconto de 10%
+			if (DiasReservados >= 10)
+			{
+				decimal desconto = valorIntegral * 10 / 100;
+				valor = valorIntegral - desconto;
+			}
 
-            return valor;
-        }
-    }
+			return valor;
+		}
+	}
 }
--- Models/Suite.cs
+++ Models/Suite.cs
@@ -1,18 +1,18 @@
 namespace DesafioProjetoHospedagem.Models
 {
-    public class Suite
-    {
-        public Suite() { }
+	public class Suite
+	{
+		public Suite() { }
 
-        public Suite(string tipoSuite, int capacidade, decimal valorDiaria)
-        {
-            TipoSuite = tipoSuite;
-            Capacidade = capacidade;
-            ValorDiaria = valorDiaria;
-        }
+		public Suite(string tipoSuite, int capacidade, decimal valorDiaria)
+		{
+			TipoSuite = tipoSuite;
+			Capacidade = capacidade;
+			ValorDiaria = valorDiaria;
+		}
 
-        public string TipoSuite { get; set; }
-        public int Capacidade { get; set; }
-        public decimal ValorDiaria { get; set; }
-    }
+		public string TipoSuite { get; set; }
+		public int Capacidade { get; set; }
+		public decimal ValorDiaria { get; set; }
+	}
 }
